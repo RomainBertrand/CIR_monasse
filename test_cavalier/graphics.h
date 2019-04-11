6fca8d8e2a1238042f20488a65600de65cd63817
Darkduv <maximin.duvillard@icloud.com>
2019-04-11 14:43:25 +0200

    T'es mauvais Jack/NeXus

---
+++ test_cavalier/graphics.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Imagine/Graphics.h>
+using namespace Imagine;
+class Case{
+public:
+    int i,j;
+    Case(int i,int j);
+    Case();
+};
+
+const int space=50;
+const int RAYON=fmax(space/4, 1);
+const Color col_robot = GREEN;
+const Color col_case = RED;
+const Color Gray=Color(200, 200, 200);
+
+
+void Coord(Case c, int&x, int&y);
+
+void DisplayGrid();
+void MakeCircle(Case c, Color col, int r = RAYON);
+void DisplayRobot(Case c);
+void ExitCase(Case c);
+void GoTo(Case c1, Case c2);
